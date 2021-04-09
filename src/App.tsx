92a23f6e7ae84ae180531df23eef5538a3f58050
nqsjd178559344 <1401444513@qq.com>
2021-04-09 17:43:09 +0800

    【修改】

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import './App.less';
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="wrapper title">
-      <header className='wrapper-header'>
-      网罗天下图书 传承中华文明
-      </header>
-      <footer className='wrapper-footer'>222</footer>
+    <div className="wrapper">
+      <Header/>
+      <footer className="wrapper-footer">222</footer>
     </div>
   );
 }
