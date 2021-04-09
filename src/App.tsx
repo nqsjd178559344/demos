86b0a4de28fa799c40d064534cfaa4fecf181deb
nqsjd178559344 <1401444513@qq.com>
2021-04-09 10:56:22 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper title">
+      <header className='wrapper-header'>
+      网罗天下图书 传承中华文明
       </header>
+      <footer className='wrapper-footer'>222</footer>
     </div>
   );
 }
