d8b350b6ba1998a81a0dd65a8ac453bb92e5743c
nqsjd178559344 <1401444513@qq.com>
2021-04-09 17:40:55 +0800

    【新增】components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import "./index.less";
+// let userInfo  = require('../../../mock/header')
+
+interface UserInfo {
+  value: string;
+}
+
+const Header = () => {
+  const user: UserInfo = { value: "" };
+  const [userInfo, setUserInfo] = React.useState([user]);
+  useEffect(() => {
+    // axios({
+    //     url:'https://6070052c85c3f0001746f3fb.mockapi.io/api/user_info',
+    //     method:'GET',
+    // }).then(res=>{
+    //     console.log(res)
+    //     // setUserInfo(res)
+    // })
+    axios
+      .get("https://6070052c85c3f0001746f3fb.mockapi.io/api/header")
+      .then(({ data }) => {
+        const [{ title }] = data;
+        setUserInfo(title);
+      });
+  }, []);
+
+  console.log(userInfo, "userInfo");
+
+  return (
+    <header className="wrapper-header">
+      <span className="title">网罗天下图书 &nbsp; 传承中华文明</span>
+      <div>
+        <dl className="user_info">
+          {userInfo &&
+            userInfo.map((i) => (
+              <dt className="user_info_li" key={i.value}>
+                {i.value}
+              </dt>
+            ))}
+        </dl>
+        <span>送至</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
