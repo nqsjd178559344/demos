fafa7aa766d7fbde2d6accf86bccd69589be9440
nqsjd178559344 <1401444513@qq.com>
2021-04-12 18:03:04 +0800

    【修改】base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Search from "./components/Search";
+import './App.less'
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
+      <Search/>
       <footer className="wrapper-footer">222</footer>
     </div>
   );
