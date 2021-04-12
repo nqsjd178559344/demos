30ae7aa520c8ddf10ead9852b28e4371569dd0cf
nqsjd178559344 <1401444513@qq.com>
2021-04-12 18:54:14 +0800

    【修改】header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,48 +1,43 @@
-import React, { useEffect ,useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Divider } from "antd";
 import "./index.less";
 // let userInfo  = require('../../../mock/header')
 
 interface UserInfo {
-  value: string;
+  title: string;
 }
 
 const Header = () => {
-  const user: UserInfo = { value: "" };
+  const user: UserInfo = { title: "" };
   const [userInfo, setUserInfo] = useState([user]);
   useEffect(() => {
-    // axios({
-    //     url:'https://6070052c85c3f0001746f3fb.mockapi.io/api/user_info',
-    //     method:'GET',
-    // }).then(res=>{
-    //     console.log(res)
-    //     // setUserInfo(res)
-    // })
     axios
       .get("https://6070052c85c3f0001746f3fb.mockapi.io/api/header")
       .then(({ data }) => {
-        const [{ title }] = data;
-        setUserInfo(title);
+        setUserInfo(data);
       });
   }, []);
-
-  console.log(userInfo, "userInfo");
 
   return (
     <header className="wrapper">
       <div className="wrapper-header">
-      <span className="title">网罗天下图书 &nbsp; 传承中华文明</span>
-      <div>
-        <dl className="user_info">
-          {userInfo &&
-            userInfo.map((i) => (
-              <dt className="user_info_li" key={i.value}>
-                {i.value}
-              </dt>
-            ))}
-        </dl>
-        <span>送至</span>
-      </div>
+        <span className="title">网罗天下图书 &nbsp; 传承中华文明</span>
+        <div className="user_info_wrapper">
+          <dl className="user_info">
+            {userInfo &&
+              userInfo.map((i) => (
+                <dt className="user_info_li" key={i.title}>
+                  {i.title}
+                </dt>
+              ))}
+          </dl>
+          <Divider type="vertical" style={{backgroundColor: '#e2c8ca'}} />
+          <span className="user_info_li">
+            送至 &nbsp;
+            {"上海"}
+          </span>
+        </div>
       </div>
     </header>
   );
