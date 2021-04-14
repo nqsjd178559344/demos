f7271a92fc2762d95406aa4fe75ed3761d9548fd
nqsjd178559344 <1401444513@qq.com>
2021-04-14 19:10:58 +0800

    feat: TodoList路由

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route,Switch,Redirect } from 'react-router-dom';
 import Home from './pages/home';
 import Count from './pages/count'
+import TodoList from './pages/todoList'
 import './App.less'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/count' component={Count}/>
+        <Route exact path='/todoList' component={TodoList}/>
         <Redirect to='/' />
       </Switch> 
     </div>
