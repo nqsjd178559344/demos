2cf6ed59bbd91487559e1e2bd575e356e6b86c2d
nqsjd178559344 <1401444513@qq.com>
2021-05-10 22:40:25 +0800

    refactor:useReducer 重构

--- src/pages/movieSearch/index.tsx
+++ src/pages/movieSearch/index.tsx
@@ -1,18 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import BScroll from "better-scroll";
 import axios from "axios";
-import './index.less';
+import "./index.less";
 import Item, { IProps } from "./components/Item";
+
+interface State {
+  loading: boolean;
+  list: any[];
+}
+
+interface Action {
+  type: string;
+  list?: any;
+}
+
+const initState = {
+  loading: true,
+  list: [],
+};
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case "SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        list: action.list,
+      };
+    case "ERROR":
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
 const MovieSearch = () => {
-  const [list, setList] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initState);
   useEffect(() => {
     axios("https://www.omdbapi.com/?s=man&apikey=4a3b711b").then(
-      ({ data: { Search } }) => {
-        console.log(Search, "Search");
-        setList(Search);
+      ({ data: { Search, Response } }) => {
+        if (Response === "True") {
+          dispatch({
+            type: "SUCCESS",
+            list: Search,
+          });
+        } else {
+          dispatch({
+            type: "ERROR",
+          });
+        }
       }
     );
   }, []);
+
+  const { list } = state;
 
   return (
     <div className="movieSearchWrapper">
